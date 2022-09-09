1b2f2f321149b66f92768ac7da704af86cf7d2a6
essakkipandi <109648500+essakkipandi@users.noreply.github.com>
2022-09-09 14:34:10 +0530

    commit

--- Main.py
+++ Main.py
@@ -3,21 +3,44 @@ class Node:
     self.data = data
     self.next = None
 
-
 class Queue:
   def __init__(self):
     self.head = None
-    self.last = None
+    self.rear = None
 
   def enqueue(self, data) -> None:
     # Write your code here
-
+    if self.rear == None:
+      self.rear =Node(data)
+      self.rear.next=None
+      self.rear.data=data
+      self.head =self.rear
+    else:
+      temp = Node(data)
+      self.rear.next=temp
+      temp.data=data
+      temp.next=None
+      self.rear=temp
+      
   def dequeue(self) -> None:
     # Write your code here
-
+    temp =self.head
+    if self.head==None:
+      return None
+    self.head = temp.next
+    if(self.head == None):
+      self.rear = None
+      
   def status(self) -> None:
     # Write your code here
-
+    temp=self.head
+    if (self.head == None and self.rear == None):
+      print("None")
+    while(temp != None):
+      print(temp.data,end="=>")
+      temp=temp.next
+      if temp==None:
+        print("None")
 
 # Do not change the following code
 queue = Queue()
